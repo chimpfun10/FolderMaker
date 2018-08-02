62ff63f09b6db535acb4540b8e87d815b4d30403
Donovan Porter <chimpfun10@gmail.com>
2018-08-01 20:36:49 -0400

    Changed the class instantiations to pointer references.

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	FolderManager fm;
+	FolderManager* fm;
 
 	if(strcmp(argv[1], "-d") == 0)
-		FolderManager fm(argv[2]);
+		fm = new FolderManager(argv[2]);
 	else
-		FolderManager fm;
+		fm = new FolderManager();
 
 	if(argc == 2)
-		fm.createFolder(argv[1]);
+		fm->createFolder(argv[1]);
 	else if(argc == 4)
-		fm.createFolder(argv[3]);
+		fm->createFolder(argv[3]);
 	else
 		cout << "Error in execution\n";
 
